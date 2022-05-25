7d0d892867bde8c0277d68a0fd9b01f775977f35
zuzannazdeb <zdebzuzanna@gmail.com>
2022-05-25 22:01:57 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int adding(int a, int b){
+        return a + b;
+    }
+    public int substracting(int a, int b){
+        return a - b;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.adding(5,8));
+    }
+}
